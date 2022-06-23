99afe29b14040e209f8f72ab3efbfaba58bb3ba4
Adarsh P V <adarshpv9@gmail.com>
2022-06-23 15:45:42 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <TestItem/>
   <TestItem data='Hello new user' msg='click here'/>
 </template>
 
