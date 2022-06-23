df7ff06fa9b0a6de35a3b72392f0920d6825c948
adarshvijayan <adarshvijayan@qburst.com>
2022-06-23 09:33:56 +0530

    basics

---
+++ src/components/TestItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Az033 {{data}}</h1>
+        <a href="adzvue.netlify.app">{{msg}}</a>
+    </div>
+</template>
+<script>
+export default {
+    name: "TestItem",
+    props:{
+        data:String,
+        msg:String
+    }
+}
+</script>
