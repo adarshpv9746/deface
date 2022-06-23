12a01ea44ef9d09190e64b8724d0eaae22a42e98
Adarsh P V <adarshpv9@gmail.com>
2022-06-23 14:12:45 +0530

    href update

--- src/components/TestItem.vue
+++ src/components/TestItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Az033 {{data}}</h1>
-        <a href="adzvue.netlify.app">{{msg}}</a>
+        <a href="/home/qburst/Desktop/orthocatapult/vue/src/components/deface.html">{{msg}}</a>
     </div>
 </template>
 <script>
