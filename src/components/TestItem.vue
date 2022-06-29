a9928b5b0998bc0f10ac9eb535cd6b4d487bfe58
Adarsh PV <33172696+adarshpv9746@users.noreply.github.com>
2022-06-29 16:40:26 +0530

    UI loader

--- src/components/TestItem.vue
+++ src/components/TestItem.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <h1>Sorry we are currently developing {{data}} {{msg}}</h1>
+        <!-- <h1>Az033 {{data}}</h1>
+        <a href="adzvue.netlify.app">{{msg}}</a> -->
+        <h3>
+        <button class="button button1" v-on:click="button('green')">Green</button>
+        <button class="button button2" v-on:click="button('blue')">Blue</button>
+        </h3>
     </div>
 </template>
 <script>
@@ -9,6 +14,25 @@ export default {
     props:{
         data:String,
         msg:String
+    }, methods:{
+        button(a){
+                alert(a+' button clicked')
+        }
     }
 }
 </script>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.button1 {background-color: #4CAF50;} /* Green */
+.button2 {background-color: #008CBA;} /* Blue */
+</style>
