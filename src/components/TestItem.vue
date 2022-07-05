b947ebb08c489a58fd85cc1a353d7f390f186a56
adarshvijayan <adarshvijayan@qburst.com>
2022-07-05 16:02:45 +0530

    show

--- src/components/TestItem.vue
+++ src/components/TestItem.vue
@@ -2,11 +2,13 @@
     <div>
         <!-- <h1>Az033 {{data}}</h1>
         <a href="adzvue.netlify.app">{{msg}}</a> -->
-        <img v-if="show" alt="Vue logo" src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif" class="center">
-        <img v-if="!walk" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1abfa455389655.59822ff82373e.gif" class="center">
-        <img v-if="!fly" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4029a055389655.59822ff823c19.gif" class="center">
-        <img v-if="!drive" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0f7cda43377507.57ecc15d6620f.gif" class="center">
-        <h1 class="testitem">
+        <h1 class="center">
+        <img v-if="show" alt="Vue logo" src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif">
+        <img v-if="!walk" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1abfa455389655.59822ff82373e.gif" >
+        <img v-if="!fly" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4029a055389655.59822ff823c19.gif" >
+        <img v-if="!drive" alt="Vue logo" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0f7cda43377507.57ecc15d6620f.gif" >
+        </h1>
+        <h2 class="testitem">
         <button class="button button1" v-if="fly" v-on:click="button('fly')">Fly</button>
         <button class="button button1" v-if="!fly" v-on:click="button('fly')">fly</button>
         <button class="button button2" v-if="drive" v-on:click="button('drive')">Drive</button>
@@ -14,7 +16,7 @@
         <button class="button button3" v-if="walk" v-on:click="button('walk')">Walk</button>
         <button class="button button3" v-if="!walk" v-on:click="button('walk')">walk</button>
 
-        </h1>
+        </h2>
     </div>
 </template>
 <script>
@@ -33,20 +35,21 @@ export default {
                     this.fly=!this.fly;
                     this.walk=true;
                     this.drive=true;
-                    this.show=!this.show;
+                    
                 }
                 else if(a=='drive'){
                     this.drive=!this.drive;
                     this.walk=true;
                     this.fly=true;
-                    this.show=!this.show;
+                    
                 }
                 else{
                     this.walk=!this.walk;
                     this.fly=true;
                     this.drive=true;
-                    this.show=!this.show;
+                    
                 }
+                this.show=false;
 
                 
         }
